a0f2cf47fa3ac8f6655380724fd08b02d2590a5e
futranbg <nhoc_lau_ca@yahoo.com>
2013-09-16 17:52:25 +0700

    Git init

---
+++ build_linaro.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+###############################################################################
+#
+#                           Kernel Build Script 
+#
+###############################################################################
+# 2011-10-24 effectivesky : modified
+# 2010-12-29 allydrop     : created
+###############################################################################
+##############################################################################
+# set toolchain
+##############################################################################
+
+export ARCH=arm
+export CROSS_COMPILE=~/Linaro-Toolchain/bin/arm-cortex_a8-linux-gnueabi-
+#export CROSS_COMPILE=arm-linux-gnueabi-
+##############################################################################
+# make zImage
+##############################################################################
+
+make -j3 zImage
+make INSTALL_MOD_PATH=output modules
+make INSTALL_MOD_PATH=output modules_install
+#mkdir -p ./obj/KERNEL_OBJ/
+#make O=./obj/KERNEL_OBJ/ msm8660-perf-EF65L_TP20_defconfig
+#make -j4 O=./obj/KERNEL_OBJ/
+
+##############################################################################
+# Copy Kernel Image
+##############################################################################
+#cp -f ./obj/KERNEL_OBJ/arch/arm/boot/zImage .
+cp -f arch/arm/boot/zImage ~/boot.img-tools/EF40S-zImage
+find output/ -name *.ko -exec mv {} output/ \;
